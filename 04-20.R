301e79e3c59a79f573b760be195b1a6c8b1d88aa
bmwoodruff <bmwoodruff@gmail.com>
2023-04-20 10:44:26 -0600

    in class mid way

--- 04-20.R
+++ 04-20.R
@@ -19,3 +19,40 @@ v
 v^2
 
 
+?plot
+
+
+
+#Plot a function
+v <- seq(-2,5)
+v
+v^2
+plot(v,v^2)
+plot(v,v^2, type = "l")
+?plot
+
+
+f <- function(x){x^2}
+f(10)
+
+x <- seq(-2,5,0.01)
+plot(x, f(x), type = "l")
+plot(f, -2,5)
+
+f <- function(x){
+  ifelse(x < 1, 2*x, 3)
+}
+
+f(-2)
+f(-1)
+f(0)
+f(1)
+f(45.6)
+
+v <- c(2,5)
+f(v)
+
+
+plot(f,-5,5)
+x <- seq(-5,5, 0.001)
+plot(x, f(x), type = "l")
